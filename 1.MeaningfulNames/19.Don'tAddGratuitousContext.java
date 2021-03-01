34e7b61be3ef13894cf48f284802b334d32755f0
christianjaena <christian.jaena@msugensan.edu.ph>
2021-03-01 10:21:04 +0800

    finalized Meaningful Names

---
+++ 1.MeaningfulNames/19.Don'tAddGratuitousContext.java
@@ -0,0 +1,13 @@
+/**
+	DON'T ADD GRATUITOUS CONTEXT
+*/
+
+
+// In an app, don't prefix every class by its abbreviation.
+
+// For example, a Todo List app, don't name every class with prefixed "TDL<class>"
+
+// Shorter names are generally better than longer ones, so long as they are clear.
+// Add no more context to a name than is necessary.
+
+// PRECISION IN NAMING IS KEY
